8ee73ac36b78e49893dd3b977acddfa804499c8e
lbelfanti <lucher06@gmail.com>
2015-05-30 01:20:21 -0300

    Agregados HTMLs para agregar, modificar y borrar películas

--- ionicApp/www/js/app.js
+++ ionicApp/www/js/app.js
@@ -1,17 +1,58 @@
-angular.module('ionicApp', ['ionic'])
+// Ionic Starter App
 
-    .controller('MainCtrl', function($scope) {
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'ionicApp' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'ionicApp.services' is found in services.js
+// 'ionicApp.controllers' is found in controllers.js
+angular.module('ionicApp', ['ionic', 'ionicApp.services', 'ionicApp.controllers'])
 
-      $scope.settingsList = [
-        { text: "Wireless", checked: true },
-        { text: "GPS", checked: false },
-        { text: "Bluetooth", checked: false }
-      ];
+    .config(function($stateProvider, $urlRouterProvider) {
 
-      $scope.pushNotificationChange = function() {
-        console.log('Push Notification Change', $scope.pushNotification.checked);
-      };
-      $scope.pushNotification = { checked: true };
-      $scope.emailNotification = 'Subscribed';
+      $stateProvider
+          .state('menu', {
+              url: "/action",
+              abstract: true,
+              templateUrl: "actions/sideMenu.html"
+          })
+          .state('menu.home', {
+              url: "/home",
+              views: {
+                  'menuContent': {
+                      templateUrl: "actions/home.html",
+                      controller: 'HomeController'
+                  }
+              }
+          })
+          .state('menu.add', {
+              url: "/add",
+              views: {
+                  'menuContent' :{
+                      templateUrl: "actions/add.html",
+                      controller: "AddController"
+                  }
+              }
+          })
+          .state('menu.modify', {
+              url: "/modify",
+              views: {
+                  'menuContent' :{
+                      templateUrl: "actions/modify.html",
+                      controller: "ModifyController"
+                  }
+              }
+          })
+          .state('menu.delete', {
+              url: "/delete",
+              views: {
+                  'menuContent' :{
+                      templateUrl: "actions/delete.html",
+                      controller: "DeleteController"
+                  }
+              }
+          });
+
+      $urlRouterProvider.otherwise("/action/home");
 
     });
+
---
+++ ionicApp/www/js/controllers.js
@@ -0,0 +1,70 @@
+angular.module('ionicApp.controllers', [])
+
+    //Controller to handle ionicSlideMenu
+    .controller('SideMenuController', function($scope, $ionicSideMenuDelegate) {
+        $scope.attendees = [
+            {firstname: 'Nicolas', lastname: 'Cage'},
+            {firstname: 'Jean-Claude', lastname: 'Van Damme'},
+            {firstname: 'Keanu', lastname: 'Reeves'},
+            {firstname: 'Steven', lastname: 'Seagal'}
+        ];
+        $scope.toggleLeft = function () {
+            $ionicSideMenuDelegate.toggleLeft();
+        };
+    })
+
+    .controller('HomeController', function($scope) {
+
+    })
+
+    .controller('AddController', function($scope) {
+
+        $scope.showForm = true;
+
+        $scope.shirtSizes = [
+            { text: 'Large', value: 'L' },
+            { text: 'Medium', value: 'M' },
+            { text: 'Small', value: 'S' }
+        ];
+
+        $scope.attendee = {};
+        $scope.submit = function() {
+            if(!$scope.attendee.firstname) {
+                alert('Info required');
+                return;
+            }
+            $scope.showForm = false;
+            $scope.attendees.push($scope.attendee);
+        };
+    })
+
+    .controller('ModifyController', function($scope) {
+
+        $scope.activity = [];
+        $scope.arrivedChange = function(attendee) {
+            var msg = attendee.firstname + ' ' + attendee.lastname;
+            msg += (!attendee.arrived ? ' has arrived, ' : ' just left, ');
+            msg += new Date().getMilliseconds();
+            $scope.activity.push(msg);
+            if($scope.activity.length > 3) {
+                $scope.activity.splice(0, 1);
+            }
+        };
+
+    })
+
+    .controller('DeleteController', function($scope) {
+
+        $scope.activity = [];
+        $scope.arrivedChange = function(attendee) {
+            var msg = attendee.firstname + ' ' + attendee.lastname;
+            msg += (!attendee.arrived ? ' has arrived, ' : ' just left, ');
+            msg += new Date().getMilliseconds();
+            $scope.activity.push(msg);
+            if($scope.activity.length > 3) {
+                $scope.activity.splice(0, 1);
+            }
+        };
+
+    });
+
